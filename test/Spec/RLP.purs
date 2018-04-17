805900728a8f158611be5cca7581e93417f867ef
blinky3713 <blinky@foam.space>
2018-04-17 15:37:13 -0400

    rlp and signature tests

---
+++ test/Spec/RLP.purs
@@ -0,0 +1,99 @@
+module UtilsSpec.RLP (rlpSpec) where
+
+import Prelude
+
+import Common (RawTransaction(..), makeTransactionMessage, mkAddress', mkHexString')
+import Data.ByteString as BS
+import Data.Maybe (Maybe(..), fromJust)
+import Data.Monoid (mempty)
+import Network.Ethereum.Utils.BigNumber (embed, pow)
+import Network.Ethereum.Utils.Keccak256 (keccak256)
+import Network.Ethereum.Utils.RLP as RLP
+import Network.Ethereum.Utils.Signatures as Sig
+import Node.Encoding (Encoding(Hex, UTF8))
+import Partial.Unsafe (unsafePartial)
+import Test.Spec (Spec, it, describe)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Runner (RunnerEffects)
+import Type.Quotient (mkQuotient)
+
+rlpSpec :: Spec (RunnerEffects ()) Unit
+rlpSpec = do
+
+  describe "RLP Spec" $ do
+
+    it "should return itself if single byte and less than 0x7f" do
+      let encoded = RLP.rlpEncode $ RLP.RLPHexString (mkHexString' "0a")
+      BS.length encoded `shouldEqual` 1
+
+    it "can match the RLP encoding for ints" $ do
+      let encoded = RLP.rlpEncode $ RLP.RLPInt 1024
+      BS.length encoded `shouldEqual` 3
+      BS.unsafeIndex encoded 0 `shouldEqual` mkQuotient 130
+      BS.unsafeIndex encoded 1 `shouldEqual` mkQuotient 4
+      BS.unsafeIndex encoded 2 `shouldEqual` mkQuotient 0
+
+    it "can match the RLP encoding for strings" $ do
+      let testString = "This function takes in a data, convert it to buffer if not, and a length for recursion"
+          testStringBS = unsafePartial fromJust $ BS.fromString testString UTF8
+          encoded = RLP.rlpEncode $ RLP.RLPByteString testStringBS
+
+      BS.unsafeIndex encoded 0 `shouldEqual` mkQuotient 184
+      BS.unsafeIndex encoded 1 `shouldEqual` mkQuotient 86
+
+    it "can match the RLP encoding for nested arrays" $ do
+      let nestedList = RLP.RLPArray
+                         [ RLP.RLPArray []
+                         , RLP.RLPArray [ RLP.RLPArray [] ]
+                         , RLP.RLPArray [ RLP.RLPArray []
+                                        , RLP.RLPArray [ RLP.RLPArray [] ]
+                                        ]
+                         ]
+          encoded = RLP.rlpEncode nestedList
+          testVal = BS.pack $ map mkQuotient [199, 192, 193, 192, 195, 192, 193, 192]
+      encoded `shouldEqual` testVal
+
+    it "can match the RLP encoding for transactions" $ do
+      let testVal = BS.pack $ map mkQuotient [193, 128]
+          encoded = RLP.rlpEncode $ RLP.RLPArray [RLP.RLPNull]
+      encoded `shouldEqual` testVal
+
+    it "can encode zero" $ do
+      let encoded = RLP.rlpEncode $ RLP.RLPByteString $ BS.pack [mkQuotient 0]
+          testVal = BS.pack [mkQuotient 0]
+      encoded `shouldEqual` testVal
+
+    it "can encode zero the int" $ do
+      let encoded = RLP.rlpEncode $ RLP.RLPInt 0
+          testVal = BS.pack [mkQuotient 128]
+      encoded `shouldEqual` testVal
+
+    it "EIP155 example" $ do
+      let rawTx = RawTransaction $
+                    { data: mempty
+                    , gas: embed 21000
+                    , gasPrice: embed 20 * (embed 10 `pow` 9)
+                    , nonce : embed 9
+                    , value : Just $ embed 10 `pow` 18
+                    , to: Just $ mkAddress' "0x3535353535353535353535353535353535353535"
+                    }
+          chainId = Sig.ChainId 1
+          rlpEncodingString = "ec098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a764000080018080"
+          hashedMessageString = "daf5a779ae972f972197303d7b574746c7ef83eadac0f2791ad23db92e4c8e53"
+          txMessage = makeTransactionMessage chainId rawTx
+      BS.toString txMessage Hex `shouldEqual` rlpEncodingString
+      BS.toString (keccak256 txMessage) Hex `shouldEqual` hashedMessageString
+
+    it "can match the RLP encoding for transactions" $ do
+      let rawTx = RawTransaction $
+                    { data: mkHexString' "d14e62b80000000000000000000000000000000000000000000000000000000000000001"
+                    , gas: embed 41669
+                    , gasPrice: embed 1000000000
+                    , nonce : embed 41
+                    , value : Nothing
+                    , to: Just $ mkAddress' "86b89c0906b111508d5caa38e2e61689a124c860"
+                    }
+          chainId = Sig.ChainId 4
+          rlpEncodingString = "f84729843b9aca0082a2c59486b89c0906b111508d5caa38e2e61689a124c86080a4d14e62b80000000000000000000000000000000000000000000000000000000000000001048080"
+          hashedMessageString = "f5bbe123594fb9f588c98b069063ce1d224917b7374c700e49ec4fce0f63f999"
+      BS.toString (makeTransactionMessage chainId rawTx) Hex `shouldEqual` rlpEncodingString
---
+++ test/Spec/Signatures.purs
@@ -0,0 +1,90 @@
+module UtilsSpec.Signatures (signatureSpec) where
+
+import Prelude
+
+import Common (RawTransaction(..), makeTransactionMessage, mkAddress', mkHexString', mkPrivateKey')
+import Control.Monad.Aff (Aff)
+import Data.Maybe (Maybe(..))
+import Network.Ethereum.Utils.BigNumber (embed)
+import Network.Ethereum.Utils.Keccak256 (keccak256)
+import Network.Ethereum.Utils.Signatures as Sig
+import Test.Spec (Spec, it, describe)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Runner (RunnerEffects)
+
+signatureSpec :: Spec (RunnerEffects ()) Unit
+signatureSpec = do
+  describe "Testing Public / PrivateKeys" $ do
+
+    it "can use the public / private key smart constructors" $ do
+      let privateKey = mkPrivateKey' "3c9229289a6125f7fdf1885a77bb12c37a8d3b4962d936f7e3084dece32a3ca1"
+          publicKey = Sig.privateToPublic privateKey
+      validatePrivatePublicPair privateKey publicKey
+
+  describe "Transaction Signing" $ do
+
+      it "can hash, sign, and recover transactions" do
+        let privateKey = mkPrivateKey' "ebd915cdf9623ca2a951c8a31f8c3277457c52dcf15ce92a825629fbc8d939cb"
+            publicKey = Sig.privateToPublic privateKey
+            address = Sig.publicToAddress publicKey
+            rawTx = RawTransaction $
+                      { data: mkHexString' "d14e62b80000000000000000000000000000000000000000000000000000000000000001"
+                      , gas: embed 41669
+                      , gasPrice: embed 1000000000
+                      , nonce : embed 41
+                      , value : Nothing
+                      , to: Just $ mkAddress' "86b89c0906b111508d5caa38e2e61689a124c860"
+                      }
+            -- this is a rinkeby transaction
+            chainId = Sig.ChainId 4
+            hashedMessage = keccak256 $ makeTransactionMessage chainId rawTx
+            sig@(Sig.Signature signature) = Sig.signMessage privateKey hashedMessage
+            Sig.Signature testSignature = Sig.removeChainIdOffset chainId $ Sig.Signature
+                                            { v: 43
+                                            , r: mkHexString' "f2458a57ea1c25f9d6220efc7128aa3ecd9b1397b158f8c29ce27a520994d03c"
+                                            , s: mkHexString' "0689a079ade60f4d1235fa866bf41e7ac6f11931d60548d082cbf7567c046cf7"
+                                            }
+        address `shouldEqual` mkAddress' "0x032392b4bdb5c7b19506131fdb8be108ce8ad995"
+        Sig.recoverSender hashedMessage sig `shouldEqual` publicKey
+        signature.v `shouldEqual` testSignature.v
+        signature.r `shouldEqual` testSignature.r
+        signature.s `shouldEqual` testSignature.s
+
+--------------------------------------------------------------------------------
+-- Helper types and functions
+--------------------------------------------------------------------------------
+
+validatePrivatePublicPair
+  :: forall r.
+     Sig.PrivateKey
+  -> Sig.PublicKey
+  -> Aff r Unit
+validatePrivatePublicPair private public = do
+  let mpublic = Sig.mkPublicKey <<< Sig.unPublicKey $ public
+  mpublic `shouldEqual` Just public
+  let mprivate = Sig.mkPrivateKey <<< Sig.unPrivateKey $ private
+  mprivate `shouldEqual` Just private
+  Sig.privateToPublic private `shouldEqual` public
+
+{-
+
+Test Transactions
+
+{
+  "blockHash": "0xd3d3fda50e1f5e7d835f12db6a8c7ad68c10735c1928315d85aa6c04cde4197a",
+  "blockNumber": 2123152,
+  "from": "0x032392b4bdb5c7b19506131fdb8be108ce8ad995",
+  "gas": 41669,
+  "gasPrice": "1000000000",
+  "hash": "0x221ea3bce6752d64f3b6330af3a2b09e225e026fd0309db04cb61814cadd99f2",
+  "input": "0xd14e62b80000000000000000000000000000000000000000000000000000000000000001",
+  "nonce": 41,
+  "to": "0x86b89c0906b111508d5caa38e2e61689a124c860",
+  "transactionIndex": 8,
+  "value": "0",
+  "v": "0x2b",
+  "r": "0xf2458a57ea1c25f9d6220efc7128aa3ecd9b1397b158f8c29ce27a520994d03c",
+  "s": "0x689a079ade60f4d1235fa866bf41e7ac6f11931d60548d082cbf7567c046cf7"
+}
+
+-}
