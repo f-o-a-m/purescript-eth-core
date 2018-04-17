8ca87a2b997bf3a4864275c257681118d7d2b13a
blinky3713 <blinky@foam.space>
2018-04-17 12:33:22 -0400

    rlp

---
+++ src/Network/Ethereum/Utils/RLP.js
@@ -0,0 +1,5 @@
+var RLP = require('rlp');
+
+exports._rlpEncode = RLP.encode;
+
+exports._rlpNull = null;
---
+++ src/Network/Ethereum/Utils/RLP.purs
@@ -0,0 +1,49 @@
+module Network.Ethereum.Utils.RLP
+  ( class RLPEncode
+  , RLPObject(..)
+  , rlpEncode
+  ) where
+
+import Prelude
+import Data.ByteString (ByteString)
+import Network.Ethereum.Utils.BigNumber (BigNumber)
+import Network.Ethereum.Utils.HexString (Address, HexString, unAddress, unHex)
+import Unsafe.Coerce (unsafeCoerce)
+
+class RLPEncode a where
+  rlpEncode :: a -> ByteString
+
+data RLPObject =
+    RLPNull
+  | RLPString String
+  | RLPHexString HexString
+  | RLPAddress Address
+  | RLPInt Int
+  | RLPBigNumber BigNumber
+  | RLPByteString ByteString
+  | RLPArray (Array RLPObject)
+
+data RLPVal = RLPVal
+
+foreign import _rlpNull :: RLPVal
+
+transRLP
+  :: RLPObject
+  -> RLPVal
+transRLP obj = case obj of
+    RLPNull -> _rlpNull
+    RLPString s -> mkRLPVal s
+    RLPInt n -> mkRLPVal n
+    RLPHexString hx -> mkRLPVal $ "0x" <> (unHex hx)
+    RLPAddress addr -> transRLP <<< RLPHexString $ unAddress addr
+    RLPBigNumber bn -> mkRLPVal bn
+    RLPByteString bs -> mkRLPVal bs
+    RLPArray as -> mkRLPVal $ map transRLP as
+  where
+    mkRLPVal :: forall a. a -> RLPVal
+    mkRLPVal = unsafeCoerce
+
+foreign import _rlpEncode :: RLPVal -> ByteString
+
+instance rlpEncodeObject :: RLPEncode RLPObject where
+  rlpEncode = _rlpEncode <<< transRLP
