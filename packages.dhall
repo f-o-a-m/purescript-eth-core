f41fddd4e1dbb16b50b9c7da4b0b2c6ca89bee08
martyall <martyall@protonmail.com>
2025-01-01 10:01:58 -0800

    using spago@next with spago.yaml

--- packages.dhall
+++
@@ -1,5 +0,0 @@
-let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.15.15-20240416/packages.dhall
-        sha256:ca727657c01cc31d0e79c2113b59126b9826f4b56d20a8193be3c725599fb754
-
-in  upstream
--- test/Main.purs
+++ test/Main.purs
@@ -8,13 +8,11 @@ import CoreSpec.Keccak256 (keccak256Spec)
 import CoreSpec.RLP (rlpSpec)
 import CoreSpec.Signatures (signatureSpec)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 main :: Effect Unit
-main = launchAff_
-  $ runSpec [ consoleReporter ]
+main = runSpecAndExitProcess [ consoleReporter ]
   $ do
       keccak256Spec
       hexSpec
